87899262109498da655f3fbe6685c7dcf63ce5cc
callum.cooper <callum.cooper@digital.hmrc.gov.uk>
2016-04-14 15:42:08 +0100

    Early setup for OpenGL using GLUT library.

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,13 +1,39 @@
 // main.cpp - Entry point to application
 
 #include <iostream>
+#include <GL\glut.h>
 
 using namespace std;
 
-int main()
+void draw()
 {
-	cout << "Hello World!" << endl;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+		glVertex3f(0.25, 0.25, 0.0);
+		glVertex3f(0.75, 0.75, 0.0);
+	glEnd();
+	glFlush();
+}
 
-	cin.ignore();
+void initialise()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+int main(int iArgc, char** cppArgv)
+{
+	glutInit(&iArgc, cppArgv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(200, 200);
+	glutCreateWindow("OpenGL");
+	initialise();
+	glutDisplayFunc(draw);
+	glutMainLoop();
+
 	return 0;
 }
